b2b16424305cee70aaa6bddf6f07e44c03ba7c05
sergey.koumirov <sergey.koumirov@gmail.com>
2018-07-28 18:09:21 +0400

    current/max jobs

--- server/ctrl/app_sap.go
+++ server/ctrl/app_sap.go
@@ -1,36 +1,23 @@
 package ctrl
 
 import (
-	"ForumPublica/server/db"
 	"ForumPublica/server/middleware"
 	"ForumPublica/server/models"
 	"ForumPublica/server/services"
-	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/jinzhu/gorm"
 )
 
 func AppSAP(c *gin.Context) {
 	raw, _ := c.Get(middleware.USER)
 	user := raw.(models.User)
 
-	var chars []models.Character
-
-	errSel := db.DB.Preload("Jobs", jobsOrder).Where("user_id=?", user.Id).Order("name").Find(&chars).Error
-
-	if errSel != nil {
-		fmt.Println("errSel", errSel)
-	}
+	chars := services.CharJobsList(user.Id)
 
 	c.Keys["chars"] = chars
 	c.Keys["timeout"] = services.GetTimeout(services.JOBS, 5)
 	c.HTML(http.StatusOK, "app/sap.html", c.Keys)
-}
-
-func jobsOrder(db *gorm.DB) *gorm.DB {
-	return db.Order("esi_jobs.end_date asc")
 }
 
 func AppSAPRefresh(c *gin.Context) {
--- server/services/jobs.go
+++ server/services/jobs.go
@@ -4,9 +4,76 @@ import (
 	"ForumPublica/server/db"
 	"ForumPublica/server/models"
 	"fmt"
+
+	"github.com/jinzhu/gorm"
 )
 
+type CharJobs struct {
+	Char       models.Character
+	MnfJobs    int32
+	MaxMnfJobs int32
+	ScnJobs    int32
+	MaxScnJobs int32
+}
+
 var JOBS = "RefreshJobs"
+
+func CharJobsList(userId int64) []CharJobs {
+	var chars []models.Character
+
+	errSel := db.DB.Preload("Jobs", jobsOrder).
+		Preload("Skills").
+		Where("user_id=?", userId).
+		Order("name").
+		Find(&chars).Error
+	if errSel != nil {
+		fmt.Println("errSel", errSel)
+	}
+
+	result := make([]CharJobs, len(chars))
+
+	mnfIds := []int32{1}
+	scnIds := []int32{3, 4, 5, 8}
+
+	for i, char := range chars {
+		result[i] = CharJobs{
+			Char:       char,
+			MnfJobs:    jobsCnt(char.Jobs, mnfIds),
+			MaxMnfJobs: maxJobs(char.Skills, 3387, 24625),
+			ScnJobs:    jobsCnt(char.Jobs, scnIds),
+			MaxScnJobs: maxJobs(char.Skills, 3406, 24624),
+		}
+	}
+
+	return result
+
+}
+
+func jobsCnt(jobs []models.Job, ids []int32) int32 {
+	result := int32(0)
+	for _, job := range jobs {
+		for _, id := range ids {
+			if job.ActivityId == id {
+				result++
+			}
+		}
+	}
+	return result
+}
+
+func maxJobs(skills []models.Skill, id1 int32, id2 int32) int32 {
+	result := int32(1)
+	for _, skill := range skills {
+		if skill.SkillId == id1 || skill.SkillId == id2 {
+			result = result + skill.Level
+		}
+	}
+	return result
+}
+
+func jobsOrder(db *gorm.DB) *gorm.DB {
+	return db.Order("esi_jobs.end_date asc")
+}
 
 func RefreshJobs(userId int64) {
 	var chars []models.Character
